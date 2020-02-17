95b69d72c35ca1eac13a85076175580263db90f5
Takafumi Suzuki <highdro0479@gmail.com>
2020-02-17 13:56:31 +0900

    modify: adjust behavior based on Intuition

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -77,3 +77,35 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class NeumorphicButton extends StatefulWidget {
+  final bool isActive;
+
+  const NeumorphicButton({Key key, this.isActive}) : super(key: key);
+
+  @override
+  _NeumorphicButtonState createState() => _NeumorphicButtonState();
+}
+
+class _NeumorphicButtonState extends State<NeumorphicButton> {
+  bool _isActive = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _isActive = !_isActive;
+        });
+      },
+      child: AnimatedNeumorphicContainer(
+        depth: _isActive ? 0.0 : 1.0,
+        color: Color(0xFFF2F2F2),
+        width: 60,
+        height: 60,
+        radius: 16,
+        child: Icon(Icons.access_time),
+      ),
+    );
+  }
+}
